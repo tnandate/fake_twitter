d168c1671e9eb2a1c5fcf49d8081cc4c0ffc3ffe
tnandate <nandate.takuya@moneyforward.co.jp>
2019-06-27 15:19:23 +0900

    stub a request for /1.1/followers

--- lib/fake_twitter/stub_app.rb
+++ lib/fake_twitter/stub_app.rb
@@ -1,6 +1,22 @@
 require 'sinatra/base'
 
-module FakeStripe
+module FakeTwitter
   class StubApp < Sinatra::Base
+    get '/1.1/followers' do
+      json_response 200, fixture('follower_ids')
+    end
+
+    private
+
+    def fixture(file_name)
+      file_path = File.join(FakeTwitter.fixture_path, "#{file_name}.json")
+      File.open(file_path, 'rb').read
+    end
+
+    def json_response(response_code, response_body)
+      content_type :json
+      status response_code
+      response_body
+    end
   end
 end
