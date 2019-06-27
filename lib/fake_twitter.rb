37f292c971030bfcb7b391222ccf79cc7c0b6374
tnandate <nandate.takuya@moneyforward.co.jp>
2019-06-27 21:23:07 +0900

    extend FakeTwitter::Configuration

--- lib/fake_twitter.rb
+++ lib/fake_twitter.rb
@@ -1,8 +1,10 @@
 require 'fake_twitter/version'
 require 'fake_twitter/initializers/webmock'
 require 'fake_twitter/stub_app'
+require 'fake_twitter/configuration'
 
 module FakeTwitter
+  extend Configuration
   def self.stub_twitter
     stub_request(:any, /api.twitter.com/).to_rack(FakeTwitter::StubApp)
   end
